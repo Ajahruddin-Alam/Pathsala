0532d487fd4fb8be4d119e00b05026b688060cf2
Ajahruddin-Alam <108749672+Ajahruddin-Alam@users.noreply.github.com>
2026-06-12 16:03:16 +0530

    push to main

--- assets/js/main.js
+++ assets/js/main.js
@@ -405,3 +405,21 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
 });
+const card =
+document.querySelector('.contact-info-card');
+
+card.addEventListener('mousemove', e => {
+
+    const rect =
+    card.getBoundingClientRect();
+
+    const x =
+    e.clientX - rect.left;
+
+    const y =
+    e.clientY - rect.top;
+
+    card.style.setProperty('--x', x + 'px');
+    card.style.setProperty('--y', y + 'px');
+
+});
